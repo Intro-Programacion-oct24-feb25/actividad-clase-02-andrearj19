f8fc5a2f8bf8f81e19b8e3cc5b4937a341662ab0
Andrea Arias <aaarias19@utpl.edu.ec>
2024-10-17 10:43:54 -0500

    trabajando en NetBeans

---
+++ Ejercicioclase002/src/ejercicioclase002/Ejercicioclase002.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicioclase002;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicioclase002 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        System.out.println("Trabajando en NetBeans");
+        
+    }
+    
+}
